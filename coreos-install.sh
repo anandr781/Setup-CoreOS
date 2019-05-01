31bec94882ff8e1be982e575c5d13e661a7377a4
Anand Ranganathan <Anand.Ranganathan@gartner.com>
2019-05-01 13:18:30 +0530

    script for bootstapping the CoreOS install

---
+++ coreos-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Enter installation device : " device_var
+wget https://github.com/coreos/container-linux-config/transpiler/v0.9.0/ct-v0.9.0-x86_64-unknown-linux-gnu
+mv ct-v0.9.0-x86_64-unknown-linux-gnu ct
+chmod a+x ct
+
+wget  https://raw.githubusercontent.com/anandr781/Setup-CoreOS/master/CoreOS-Consul.yml
+./ct --in-file=CoreOS-Consul.yml --out-file=ignition
+
+sudo coreos-install -d $device_var -i ignition -C stable
