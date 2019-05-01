72d3439ef69556c04a7cf275cf1ee60c12476b48
Anand Ranganathan <Anand.Ranganathan@gartner.com>
2019-05-01 13:38:21 +0530

    rectified the URL path for the ct tool

--- coreos-install.sh
+++ coreos-install.sh
@@ -4,7 +4,7 @@ read -p "Enter installation device : " device_var
 wget https://github.com/coreos/container-linux-config-transpiler/releases/download/v0.9.0/ct-v0.9.0-x86_64-unknown-linux-gnu
 mv ct-v0.9.0-x86_64-unknown-linux-gnu ct
 chmod a+x ct
-
+ 
 wget  https://raw.githubusercontent.com/anandr781/Setup-CoreOS/master/CoreOS-Consul.yml
 ./ct --in-file=CoreOS-Consul.yml --out-file=ignition
 
